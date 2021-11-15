39668b5424b24419dd409f83b27f569eda6bc128
Akira <man4781747@gmail.com>
2021-11-15 09:42:12 +0000

    修改細部參數 符合HAP使用

--- Volume/DjangoServer/TreeStudio/TreeStudioAPIs/static/TreeStudioAPIs/Misc/Params.js
+++ Volume/DjangoServer/TreeStudio/TreeStudioAPIs/static/TreeStudioAPIs/Misc/Params.js
@@ -1,2 +1,5 @@
 var airjobURL = "http://35.194.167.48:8000/"
 var hive_table_status_URL = "http://35.194.167.48:8801/TreeStudioAPIs/Test_hive_table_status_URL/"
+
+
+var jupyterURL = "http://88.248.13.77"
