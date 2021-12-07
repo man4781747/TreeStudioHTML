81ce6ab47b6efaa43be613974dbe7d5ddb483e3c
Akira <man4781747@gmail.com>
2021-12-07 09:28:38 +0000

    forOutside版本出版

--- Volume/DjangoServer/TreeStudio/TreeStudioAPIs/static/TreeStudioAPIs/MainToolBox/VueSetting.js
+++ Volume/DjangoServer/TreeStudio/TreeStudioAPIs/static/TreeStudioAPIs/MainToolBox/VueSetting.js
@@ -15,68 +15,6 @@ var Vue_mainToolBox =  new Vue({
                 'Type': 'Page',
                 'icon': 'fas fa-bullhorn',
             },
-            'HAP_Hue': {
-                'Name': 'HAP Hue',
-                'Type': 'URL',
-                'URL': HueURL,
-                'icon': 'fas fa-poll',
-                'title': 'HAP Hue',
-                'content': 'Hue為Hadoop的HAP環境中，進行資料探索的工具，若想獲得詳細資訊，請至Wiki搜尋 "Hue"',
-            },
-            'HAP_Jupyter': {
-                'Name': 'HAP Jupyter',
-                'Type': 'Page',
-                'icon': 'fab fa-python',
-            },
-            'HAP_AIRJOB': {
-                'Name': 'HAP AIRJOB',
-                'Type': 'List', 
-                'opened': false,
-                'icon': 'far fa-calendar-alt',
-                'height' : 0,
-                'id' : 'main-toolbox-list-HAP_AIRJOB',
-                // 'svg': 'http://34.135.113.78:9000/static/dist/img/AirflowIcon-white-30-rotate-new.svg',
-                'List': [
-                    {
-                        'Name': '數據經營部',
-                        'Type': 'URL',
-                        'URL': airjobURL + 'AIRJOB/9h000/',
-                        'icon': 'far fa-circle',
-                        'title': 'AIRJOB: 數據經營部',
-                        'content': 'AIRJOB為以Airflow為底層延伸設計的自主排程系統。',
-                    },
-                    {
-                        'Name': '客戶智能科',
-                        'Type': 'URL',
-                        'URL': airjobURL + 'AIRJOB/9h001/',
-                        'icon': 'far fa-circle',
-                        'title': 'AIRJOB: 客戶智能科',
-                        'content': 'AIRJOB為以Airflow為底層延伸設計的自主排程系統。',
-                    },
-                    {
-                        'Name': '商業智能科',
-                        'Type': 'URL',
-                        'URL': airjobURL + 'AIRJOB/9h002/',
-                        'icon': 'far fa-circle',
-                        'title': 'AIRJOB: 商業智能科',
-                        'content': 'AIRJOB為以Airflow為底層延伸設計的自主排程系統。',
-                    },
-                ]
-            },
-            'HAP_Table': {
-                'Name': 'HAP 資料表查詢',
-                'Type': 'Page',
-                'icon': 'fas fa-table',
-            },
-            'HAP_Yarn': {
-                'Name': 'HAP Yarn 系統',
-                'Type': 'URL',
-                'URL': YarnURL,
-                'icon': 'fas fa-tachometer-alt',
-                'title': 'HAP Yarn 系統',
-                'content': 'HAP Yarn 系統',
-            },
-            
         },
     },
 
@@ -101,32 +39,6 @@ var Vue_mainToolBox =  new Vue({
                 }
             } else {
                 let element = document.getElementById("home-grid");
-                element.style.display = 'none';
-            } 
-
-            if (S_page == 'HAP_Table'){
-                let element = document.getElementById("hive-searcher");
-                element.style.display = '';
-            } else {
-                let element = document.getElementById("hive-searcher");
-                element.style.display = 'none';
-            } 
-
-            if (S_page == 'HAP_Jupyter'){
-                let element = document.getElementById("hap-jupyter-window");
-                element.style.display = '';
-                Vue_JupyterWindow.updateCustomerJupyterInfos()
-            } else {
-                let element = document.getElementById("hap-jupyter-window");
-                element.style.display = 'none';
-            } 
-
-            if (S_page == 'System.Jupyter 控制'){
-                let element = document.getElementById("hap-jupyter-ctrl-window");
-                element.style.display = '';
-                Vue_JupyterCtrlWindow.updateCustomerJupyterInfos()
-            } else {
-                let element = document.getElementById("hap-jupyter-ctrl-window");
                 element.style.display = 'none';
             } 
             if (S_page == 'System.公告管理系統'){
@@ -189,30 +101,9 @@ var Vue_mainToolBox =  new Vue({
                         'id' : 'main-toolbox-list-System',
                         'List': [
                             {
-                                'Name': 'Jupyter 控制',
-                                'Type': 'Page',
-                                'icon': 'far fa-circle',
-                            },
-                            {
                                 'Name': '公告管理系統',
                                 'Type': 'Page',
                                 'icon': 'far fa-circle',
-                            },
-                            {
-                                'Name': 'MySQL管理平台',
-                                'Type': 'URL',
-                                'URL': 'http://88.248.13.77:8897',
-                                'icon': 'far fa-circle',
-                                'title': 'MySQL管理平台',
-                                'content': 'phpMyAdmin 是一個以PHP為基礎，以Web-Base方式架構在網站主機上的MySQL的資料庫管理工具，讓管理者可用Web介面管理MySQL資料庫。 人壽HAP排程系統Airflow，以MySQL資料庫做為搭建基底，在營運環境除了提供Airflow使用，亦提供為HIVE資料表即時記錄，供Tree studio前端畫面使用。',
-                            },
-                            {
-                                'Name': 'PostgreSQL管理平台',
-                                'Type': 'URL',
-                                'URL': 'http://88.248.13.77:8892',
-                                'icon': 'far fa-circle',
-                                'title': 'PostgreSQL管理平台',
-                                'content': 'pgAdmin 是一個管理 PostgreSQL 的開源圖形化工具，支援多種平台。目前主要存取Jupyter Service環境資訊，以及提供Tree studio使用。',
                             },
                         ],
                     }
